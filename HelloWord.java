3bf132de346474983bcbb05bece4449992eb24ce
dharanesha <dharani.thinkdifferent@gmail.com>
2018-02-16 13:27:07 +0530

    hai

--- HelloWord.java
+++ HelloWord.java
@@ -1,6 +1,6 @@
 public class HelloWord {
    public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
+      // Prints "Hello, World" in the window.
       System.out.println("Hello, World");
    }
 }
